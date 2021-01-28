b82ae54bca9e0806c5a87da47b1eae23b180642d
Jake Hansen <jakedhansen@outlook.com>
2021-01-28 12:27:23 -0600

    Add ability to read configuration file

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/jake-hansen/agora
 go 1.15
 
 require (
-	github.com/spf13/viper v1.7.1 // indirect
+	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
 )
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/jake-hansen/agora/config"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello")
+	var environment *string = flag.String("e", "dev", "environment to run in")
+	flag.Usage = func() {
+		fmt.Println("Usage: serve -e {environment}")
+		os.Exit(1)
+	}
+	flag.Parse()
+	config.Init(*environment)
 }
