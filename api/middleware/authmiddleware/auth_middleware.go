5ee7699fbfff1b1933a727a9a896a9a0cefa13f1
Jake Hansen <jakedhansen@outlook.com>
2021-02-06 07:42:04 -0600

    Refactor auth middleware to use parse token func
    Update documentation.

--- api/middleware/authmiddleware/auth_middleware.go
+++ api/middleware/authmiddleware/auth_middleware.go
@@ -4,22 +4,34 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jake-hansen/agora/api"
 	"github.com/jake-hansen/agora/domain"
+	"net/http"
 	"strings"
 )
 
+// ParseTokenFunc defines a function which parses an HTTP request for
+// an authorization token.
+type ParseTokenFunc = func(r *http.Request) domain.Token
+
+// AuthMiddleware handles authentication by using AuthService to determine if
+// a request is authenticated.
 type AuthMiddleware struct {
 	AuthService		*domain.AuthService
+	ParseToken		ParseTokenFunc
 }
 
-func New(authService *domain.AuthService) *AuthMiddleware {
-	return &AuthMiddleware{AuthService: authService}
+// New returns a new AuthMiddleware configured with the specified AuthService and ParseTokenFunc.
+func New(authService *domain.AuthService, parseTokenFunc ParseTokenFunc) *AuthMiddleware {
+	return &AuthMiddleware{
+		AuthService: authService,
+		ParseToken:  parseTokenFunc,
+	}
 }
 
+// HandleAuth checks a request for valid authentication using the token obtained
+// from the ParseToken function against the AuthService.
 func (a *AuthMiddleware) HandleAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		t := c.GetHeader("Authorization")
-		splitToken := strings.Split(t, "Bearer ")
-		token := domain.Token{Value: splitToken[1]}
+		token := a.ParseToken(c.Request)
 		_, err := (*a.AuthService).IsAuthenticated(token)
 		if err != nil {
 			apiError := &api.APIError{
@@ -32,4 +44,13 @@ func (a *AuthMiddleware) HandleAuth() gin.HandlerFunc {
 			c.Next()
 		}
 	}
+}
+
+// getTokenFromBearerHeader parses the Authorization header for a Bearer token
+// and returns the parsed result as a domain.Token.
+func getTokenFromBearerHeader(r*http.Request) domain.Token {
+	t := r.Header.Get("Authorization")
+	splitToken := strings.Split(t, "Bearer ")
+	token := domain.Token{Value: splitToken[1]}
+	return token
 }
--- api/middleware/authmiddleware/provider.go
+++ api/middleware/authmiddleware/provider.go
@@ -5,11 +5,24 @@ import (
 	"github.com/jake-hansen/agora/domain"
 )
 
-func Provide(authService domain.AuthService) *AuthMiddleware {
-	return New(&authService)
+// Provide returns an AuthMiddleware configured with the provided AuthService and ParsTokenFunc.
+func Provide(authService domain.AuthService, parseToken ParseTokenFunc) *AuthMiddleware {
+	return New(&authService, parseToken)
+}
+
+// ProvideAuthorizationHeaderParser returns a ParseTokenFunc which parses the Authorization
+// header for a Bearer token.
+func ProvideAuthorizationHeaderParser() ParseTokenFunc {
+	return getTokenFromBearerHeader
 }
 
 var (
-	ProviderProductionSet = wire.NewSet(Provide)
+	// ProviderNoParserSet AuthMiddleware with no AuthService or ParseTokenFunc
+	ProviderNoParserSet = wire.NewSet(Provide)
+
+	// ProviderAuthorizationParserSet AuthMiddleware with authorization header parser and no AuthService
+	ProviderAuthorizationParserSet = wire.NewSet(Provide, ProvideAuthorizationHeaderParser)
+
+	// ProviderTestSet AuthMiddleware with no AuthService or ParseTokenFunc
 	ProviderTestSet = wire.NewSet(Provide)
 )
