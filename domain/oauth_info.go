e6977c213894ee335fd49bb84c9db4a706bfb134
Jake Hansen <jakedhansen@outlook.com>
2021-02-15 19:10:36 -0600

    Add repo and wire injector for OAuthInfo

---
+++ domain/oauth_info.go
@@ -0,0 +1,25 @@
+package domain
+
+import "gorm.io/gorm"
+
+type OAuthInfo struct {
+	gorm.Model
+	UserID			  uint
+	MeetingProviderID uint
+	AccessToken		  string
+	RefreshToken 	  string
+}
+
+type OAuthInfoRepository interface {
+	Create(oauthToken *OAuthInfo) (uint, error)
+	GetAll() ([]*OAuthInfo, error)
+	GetByID(ID uint) (*OAuthInfo, error)
+	GetAllByMeetingProviderId(providerID uint) ([]*OAuthInfo, error)
+	GetAllByUserID(userID uint) ([]*OAuthInfo, error)
+	Update(oauthToken *OAuthInfo) error
+	Delete(ID uint) error
+}
+
+type OAuthTokenService interface {
+
+}
