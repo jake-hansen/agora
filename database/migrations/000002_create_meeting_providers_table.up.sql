043a93215dec45e2aee773b0389f6912f29d230b
Jake Hansen <jakedhansen@outlook.com>
2021-02-17 12:29:21 -0600

    Add timestamps for created, updated, and deleted
    Rename meeting_service_id field to meeting_provider_id.

--- database/migrations/000002_create_meeting_providers_table.up.sql
+++ database/migrations/000002_create_meeting_providers_table.up.sql
@@ -1,6 +1,9 @@
 create table if not exists meeting_providers
 (
     id int auto_increment primary key,
+    deleted_at timestamp null,
+	created_at timestamp not null,
+	updated_at timestamp not null,
     name varchar(100) not null,
     constraint meeting_providers_name_uindex unique (name)
 )
