d5e591ef398a549434e8e97eb0b9cea054ca4447
Jake Hansen <jakedhansen@outlook.com>
2021-02-28 22:59:56 -0600

    Update documentation for Loader
    Change slice of MeetingPlatforms to
    meetingplatformservice.ConfiguredPlatforms for consistency.

--- database/loader/db_loader.go
+++ database/loader/db_loader.go
@@ -1,22 +1,31 @@
 package loader
 
-import "github.com/jake-hansen/agora/domain"
+import (
+	"github.com/jake-hansen/agora/domain"
+	"github.com/jake-hansen/agora/services/meetingplatformservice"
+)
 
+// MeetingPlatformLoader provides functions for loading already configured
+// MeetingPlatforms into a MeetingPlatformRepository.
 type MeetingPlatformLoader struct {
 	PlatformRepo	domain.MeetingPlatformRepository
-	ConfiguredPlatforms	[]*domain.MeetingPlatform
+	ConfiguredPlatforms	meetingplatformservice.ConfiguredPlatforms
 }
 
+// Loader provides functions for loading data into repositories.
 type Loader struct {
 	meetingPlatformLoader *MeetingPlatformLoader
 }
 
+// NewLoader returns a new Loader configured with the provided MeetingPlatformLoader.
 func NewLoader(meetingPlatformLoader *MeetingPlatformLoader) *Loader {
 	return &Loader{
 		meetingPlatformLoader: meetingPlatformLoader,
 	}
 }
 
+// NewMeetingPlatformLoader returns a new MeetingPlatformLoader configured with the provided
+// MeetingPlatformRepository and configured MeetingPlatforms.
 func NewMeetingPlatformLoader(repository domain.MeetingPlatformRepository, configuredPlatforms []*domain.MeetingPlatform) *MeetingPlatformLoader {
 	return &MeetingPlatformLoader{
 		PlatformRepo:        repository,
@@ -24,10 +33,14 @@ func NewMeetingPlatformLoader(repository domain.MeetingPlatformRepository, confi
 	}
 }
 
+// Load begins loading data into each configured repository.
 func (l *Loader) Load() error {
 	return l.meetingPlatformLoader.load()
 }
 
+// load loads the configured MeetingPlatforms into the MeetingPlatformRepository.
+// If a MeetingPlatform already exists (matching by name only) then that platform
+// is not loaded again.
 func (m *MeetingPlatformLoader) load() error {
 	platforms, err := m.PlatformRepo.GetAll()
 	if err != nil {
