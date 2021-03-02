c78227d1918c76ba3fa448abc2c365a8eb8650c2
Jake Hansen <jakedhansen@outlook.com>
2021-03-01 20:39:46 -0600

    Add start to meeting domain model

---
+++ domain/meeting.go
@@ -0,0 +1,11 @@
+package domain
+
+import "time"
+
+// Meeting represents a meeting on a MeetingPlatform.
+type Meeting struct {
+	Title       string
+	StartTime   time.Time
+	Duration    time.Duration
+	Description string
+}
